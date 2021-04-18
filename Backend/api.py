7e6b0bb26590f57deb062f14312551e70868d33e
safeer2978 <safeerkhan2978@gmail.com>
2021-04-18 16:37:22 +0530

    initial progress

---
+++ Backend/api.py
@@ -0,0 +1,107 @@
+import flask
+from flask import Flask,jsonify,request,make_response,url_for,redirect
+import requests, json
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
+import matplotlib
+import scipy
+import scipy.ndimage
+import matplotlib.pyplot as plt 
+import numpy as np 
+from PIL import Image
+import scipy.misc
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# The below two are visualization libraires
+import matplotlib.pyplot as plt
+import seaborn as sns 
+# for calculating interval
+from time import time
+from sklearn.cluster import KMeans 
+from skimage import io
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/demo', methods=['POST'])
+def demo():
+    lol = request.json['lol']
+    return lol
+@app.route('/pca', methods=['POST'])
+def pca():
+    file_path = request.json['file_path']
+    file_path = file_path.replace(" ", "\\")
+    # IMPORTING IMAGE USING SCIPY AND TAKING R,G,B COMPONENTS
+    a = matplotlib.pyplot.imread(file_path)
+    a_np = np.array(a)
+    a_r = a_np[:,:,0]
+    a_g = a_np[:,:,1]
+    a_b = a_np[:,:,2]
+
+    a_r_recon, a_g_recon, a_b_recon = comp_2d(a_r), comp_2d(a_g), comp_2d(a_b) # RECONSTRUCTING R,G,B COMPONENTS SEPARATELY
+    recon_color_img = np.dstack((a_r_recon, a_g_recon, a_b_recon)) # COMBINING R.G,B COMPONENTS TO PRODUCE COLOR IMAGE
+    recon_color_img = Image.fromarray(recon_color_img)
+    #recon_color_img.show()
+    newfilepath=file_path[:-4]
+    recon_color_img.save(newfilepath+"-compressed.png")
+    return "Guess its done?"
+
+def comp_2d(image_2d): # FUNCTION FOR RECONSTRUCTING 2D MATRIX USING PCA
+    cov_mat = image_2d - np.mean(image_2d , axis = 0).T
+    eig_val, eig_vec = np.linalg.eigh(np.cov(cov_mat)) # USING "eigh", SO THAT PROPRTIES OF HERMITIAN MATRIX CAN BE USED
+    p = np.size(eig_vec, axis =1)
+    idx = np.argsort(eig_val)
+    idx = idx[::-1]
+    eig_vec = eig_vec[:,idx]
+    eig_val = eig_val[idx]
+    numpc = 50 # THIS IS NUMBER OF PRINCIPAL COMPONENTS, YOU CAN CHANGE IT AND SEE RESULTS
+    if numpc <p or numpc >0:
+        eig_vec = eig_vec[:, range(numpc)]
+    score = np.dot(eig_vec.T, cov_mat)
+    recon = np.dot(eig_vec, score) + np.mean(image_2d, axis = 0).T # SOME NORMALIZATION CAN BE USED TO MAKE IMAGE QUALITY BETTER
+    recon_img_mat = np.uint8(np.absolute(recon)) # TO CONTROL COMPLEX EIGENVALUES
+    return recon_img_mat
+
+def recreate_image(centroids, labels, w, h):
+    # centroids variable are calculated from the flattened image
+    # centroids: w*h, d 
+    # so each row depicts the values per depth
+    d = centroids.shape[1]
+    image = np.zeros((w, h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            # filling values in new image with centroid values
+            image[i][j] = centroids[labels[label_idx]]
+            label_idx += 1
+    return image
+
+
+@app.route('/kMeans', methods=['POST'])
+def kMeans():
+    file_path = request.json['file_path']
+    file_path = file_path.replace(" ", "\\")
+    img_original = io.imread(file_path)
+    img = np.array(img_original,dtype=float) / 255
+    # Save the dimensions, we will be need them later
+    w, h, d = original_shape = img.shape
+    image_array = img.reshape(-1,d)
+    t0 = time()
+    kmeans64 = KMeans(n_clusters = 16,random_state=42,verbose=2,n_jobs=-1).fit(image_array)
+    labels64 = kmeans64.labels_
+    #print('Within cluster sum of square error for'+str( {n_colours[0]})+' clusters = '+str({round(kmeans64.inertia_,2)}))
+    newfilepath=file_path[:-4]
+    plt.imsave(newfilepath+"-compressed-kmeans.png", recreate_image(kmeans64.cluster_centers_, labels64, w, h))
+
+    return "idk when itll be done.."
+
+app.run()
---
+++ Frontend/src/App.js
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import {RadioGroup, Radio} from "react-radio-group"
+import "./App.css";
+import { useSpring, animated } from "react-spring";
+
+function App() {
+
+  const [file_path, setFilePath] = useState("343");
+  const [param, setParam] = useState("");
+
+
+  const [registrationFormStatus, setRegistartionFormStatus] = useState(false);
+  const loginProps = useSpring({ 
+    left: registrationFormStatus ? -500 : 0, // Login form sliding positions
+  });
+  const registerProps = useSpring({
+    left: registrationFormStatus ? 0 : 500, // Register form sliding positions 
+  });
+
+  const loginBtnProps = useSpring({
+    borderBottom: registrationFormStatus 
+      ? "solid 0px transparent"
+      : "solid 2px #1059FF",  //Animate bottom border of login button
+  });
+  const registerBtnProps = useSpring({
+    borderBottom: registrationFormStatus
+      ? "solid 2px #1059FF"
+      : "solid 0px transparent", //Animate bottom border of register button
+  });
+
+  function registerClicked() {
+    setRegistartionFormStatus(true);
+  }
+  function loginClicked() {
+    setRegistartionFormStatus(false);
+  }
+
+  return (
+    <div>
+  <div>
+    <h1 style={registerBtnProps}>Hello</h1>
+  </div>
+  <div className="login-register-wrapper">
+      <div className="nav-buttons">
+        <animated.button
+          onClick={loginClicked}
+          id="loginBtn"
+          style={loginBtnProps}
+        >
+          PCA
+        </animated.button>
+        <animated.button
+          onClick={registerClicked}
+          id="registerBtn"
+          style={registerBtnProps}
+        >
+          K Means
+        </animated.button>
+      </div>
+      <div className="form-group">
+        <animated.form action="" id="loginform" style={loginProps}>
+        <h1>Principal Component Analysis</h1>
+          <InputFile/>
+          <LoginForm />
+        </animated.form>
+        <animated.form action="" id="registerform" style={registerProps}>
+        <h1>K-Means Algorithm</h1>
+          <InputFile/>
+          <RegisterForm />
+        </animated.form>
+      </div>
+    </div>
+    </div>
+  );
+
+
+function LoginForm() {
+  return (
+    <React.Fragment>
+      
+      <h2 style={{padding:5}}>Select number of Principal components</h2>
+      <RadioGroup name="fruit" selectedValue={param} onChange={(e)=>{
+        setParam(25) 
+        console.log(e.value)}} style={{ display: 'flex' , width: 'auto', marginLeft:120}}>
+        <Radio value={"25"} style={{ width: 'auto' }}/><h3 style={{padding:5}} >25</h3>
+        <Radio value={50} style={{ width: 'auto' }}/><h3 style={{padding:5}} >50</h3>
+        <Radio value={75} style={{ width: 'auto' }}/><h3 style={{padding:5}} >75</h3>
+        <Radio value={100} style={{ width: 'auto' }}/><h3 style={{padding:5}} >100</h3>
+      </RadioGroup>
+      <input type="button" value="submit" className="submit" onClick={()=>{
+        console.log(file_path)
+        console.log(param)
+      }}/>
+    </React.Fragment>
+  );
+}
+
+function InputFile(){
+  return (
+    <div>
+      <h4 style={{padding:5}}>Select your image to be compressed</h4>
+      <input style={{paddingTop: 6}} type='file' id="image" onChange={
+        (e)=>{
+        setFilePath(e.target.value)
+        console.log(e.target.value)
+        console.log(file_path)
+      }}/>
+      <table style={{marginLeft:120}}>
+        <tr>
+          <tc>Image size:
+            <h2 value={file_path}/>
+          </tc>
+          <tc>123</tc>
+        </tr>
+        <tr>
+          <tc>Image dimensions:</tc>
+          <tc>123</tc>
+        </tr>
+        <tr>
+          <tc>Image format:</tc>
+          <tc>123</tc>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+function RegisterForm() {
+  return (
+    <React.Fragment>
+      <h2 style={{padding:5}}>Select number of Clusters </h2>
+      <h3 style={{paddingLeft:5}}>Color Bit Rate</h3>
+      <RadioGroup name="fruit" style={{ display: 'flex' , width: 'auto', marginLeft:130}}>
+        <Radio value={16} style={{ width: 'auto' }}/><h3 style={{padding:5}} onChange={setParam(16)}>16</h3>
+        <Radio value={32} style={{ width: 'auto' }}/><h3 style={{padding:5}} onClick={setParam(32)}>32</h3>
+        <Radio value={64} style={{ width: 'auto' }}/><h3 style={{padding:5}} onClick={setParam(64)}>64</h3>
+      </RadioGroup>
+      <input  value="send" className="submit" />
+      <input  value="result" className="submit" style={{marginLeft:5}}/>
+    </React.Fragment>
+  );
+}
+}
+
+export default App;
